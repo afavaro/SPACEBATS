cae921efcae1f2b0ff7b4d15210b42d56fac8135
Jeremy Keeshin <jkeesh@gmail.com>
2011-03-08 22:00:59 -0800

    small changes

--- Main.cpp
+++ Main.cpp
@@ -191,16 +191,14 @@ void renderFrame() {
 	motionBlur->unbind();
 	
 	int frames = frameCounter < NUM_MOTION_BLUR_FRAMES ? frameCounter : NUM_MOTION_BLUR_FRAMES;
-	cout << frames << " frames" << endl;
-	float val = 1.0 / frames;
-	cout << "val per" << val << endl;
+	//float val = 1.0 / frames;
 
 	glClearColor(1.0, 0.0, 0.0, 1.0);		
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	
 	setupLights();
 	
-	spaceship.useShader(toonShader);
-	spaceship.render(FINAL_PASS, normalsBuffer);
+//	spaceship.useShader(blurShader);
+//	spaceship.render(BLUR_PASS, normalsBuffer);
 
 }
--- shaders/blur.frag.glsl
+++ shaders/blur.frag.glsl
@@ -3,10 +3,11 @@ uniform sampler2D frame2;
 uniform sampler2D frame3;
 uniform sampler2D frame4;
 
-varying vec3 position;
+varying vec3 clipPosition;
 
 void main() {
-
+	clipPosition /= clipPosition.w;	
+	vec2 normcoord = 0.5 * clipPosition.xy + vec2(0.5, 0.5);
 
 	// This actually writes to the frame buffer
 	gl_FragColor = vec4(diffuse + specular + ambient, 1);
