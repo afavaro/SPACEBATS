11e9f63f096d8ea47ead19f696b9ae5b978eca0d
Jeremy Keeshin <jkeesh@gmail.com>
2011-03-13 15:58:07 -0700

    improving boost bar

--- Scoreboard.cpp
+++ Scoreboard.cpp
@@ -1,6 +1,9 @@
 #include "Scoreboard.h"
 #include "Framework.h"
 
+const float MAX_SCORE = 200;
+const float MAX_HEIGHT = 400;
+
 Scoreboard::Scoreboard(sf::RenderWindow* window, Shader* shader){
 	this->window = window;
 	this->shader = shader;
@@ -13,40 +16,17 @@ Scoreboard::~Scoreboard(){
 
 
 void Scoreboard::render(){
-//	glClear(GL_DEPTH_BUFFER_BIT );
-//	glDisable(GL_LIGHTING);
-//	glDisable(GL_TEXTURE_2D);
-//	
-//	glUseProgram(0);
-//	glViewport(0,0,300,60);
-//	
-//	glMatrixMode(GL_MODELVIEW);
-//	glLoadIdentity();
-//	
-//	glMatrixMode(GL_PROJECTION);
-//	glLoadIdentity();
-//	
-//	gluOrtho2D(0.0,1.0,0.0,1.0);
-	
-	
-//	glColor4f(1.0, 0.0, 0.0, 1.0);
-//	
-//	glBegin(GL_QUADS);
-//	glVertex2f(0.0,0.0);
-//	glVertex2f(0.0,1.0);
-//	glVertex2f(1.0,1.0);
-//	glVertex2f(1.0,0.0);
-//	glEnd();
-
-	
-	
 //	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 //	sf::String scoreString("50", sf::Font::GetDefaultFont(), 30.f);
 //	scoreString.SetColor(sf::Color(200, 0,0));
 //	scoreString.SetPosition(50.f, 580.f);
 //	window->Draw(scoreString);
 	
-	glViewport(0,0, 100,100);
+	
+	float height = MAX_HEIGHT * score / MAX_SCORE;
+	if(height < 5) height = 5;
+	if(height > 200) height = MAX_HEIGHT;
+	glViewport(0,0,50,height);
 	
 	glUseProgram(shader->programID());
 	
@@ -63,8 +43,7 @@ void Scoreboard::render(){
 	glVertexAttrib2f(pos, 1.0, -1.0);
 	glVertexAttrib2f(pos, 1.0, 1.0);
 	glVertexAttrib2f(pos, -1.0, 1.0);
-	glEnd();
-	
+	glEnd();	
 }
 
 void Scoreboard::print(){
